78c0cf012038fb285020a7b3107380f1ce4b5333
João Vitor Scodeler de Souza <127458435+mytimeisnowww@users.noreply.github.com>
2024-03-12 23:13:48 -0300

    Relatorio 02

---
+++ Relatório 02/Aula.js
@@ -0,0 +1,19 @@
+{
+  "aulas": [
+    {
+      "professor": "Maria Silva",
+      "assunto": "Matemática",
+      "alunos": ["Ana", "João", "Pedro", "Mariana"]
+    },
+    {
+      "professor": "Carlos Oliveira",
+      "assunto": "História",
+      "alunos": ["Paula", "Rafael", "Bruna", "Lucas"]
+    },
+    {
+      "professor": "Luana Costa",
+      "assunto": "Ciências",
+      "alunos": ["Fernanda", "Rodrigo", "Camila", "Gustavo"]
+    }
+  ]
+}
